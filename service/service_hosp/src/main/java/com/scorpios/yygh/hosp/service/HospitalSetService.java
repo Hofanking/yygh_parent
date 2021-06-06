9c55c6e30b1e0f7b5690700a18d8a4474b305956
scorpios <1435513775@qq.com>
2021-06-06 10:47:22 +0800

    基础结构

---
+++ service/service_hosp/src/main/java/com/scorpios/yygh/hosp/service/HospitalSetService.java
@@ -0,0 +1,8 @@
+package com.scorpios.yygh.hosp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.scorpios.yygh.model.hosp.HospitalSet;
+
+
+public interface HospitalSetService extends IService<HospitalSet> {
+}
